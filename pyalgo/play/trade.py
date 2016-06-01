88b7a9b9b722cae68bf3b3b83bc11e58c5b8fb99
alex <alexandre.miot@sgcib.com>
2016-06-01 13:25:38 +0200

    trade seems to work needs improvement in init

--- pyalgo/play/trade.py
+++ pyalgo/play/trade.py
@@ -16,39 +16,89 @@ at 15
 
 import numpy as np
 
-def single_trade(prices):
-    # outputs best single trade from index n of price array
-    # brute force for now but could do better
-    # O(n**2)
+def bf_left(prices):
+    # pretty dire implemetation
+    # O(n**3)
+    # for fine grained proof e.g. https://en.wikipedia.org/wiki/Faulhaber%27s_formula
     l = len(prices)
-    s = prices[:]
-    s[-1] = 0
-    for i in range(l-1):
-        m = max((prices[j] for j in range(i+1, l)))
-        s[i] = m - s[i]
+    s = [None] * l
+    for e in range(1, l):
+        m = []
+        for i_s in range(0, e):
+            last_max = max((prices[j] for j in range(i_s+1, e+1)))
+            m.append(last_max - prices[i_s])
+        s[e] = max(m)
     return s
 
-def bf_max(prices):
+def bf_right(prices):
+    # exactly like left but here
+    # the bound is given by the starting point
     l = len(prices)
-    for i in range():
-        m = max((prices[j] for j in range(i+1, l)))
-        s[i] = m - s[i]
+    s = [None] * l
+    for b_s in range(0, l-1):
+        m = []
+        for i_s in range(b_s, l-1):
+            last_max = max((prices[j] for j in range(i_s+1, l)))
+            m.append(last_max - prices[i_s])
+        s[b_s] = max(m)
+    return s
+
+def single_best(prices, index_start, index_end):
+    # pretty horrible implementation
+    # brute force could be improved
+    # l = index_end - index_start + 1
+    # O(l**2)
+    res = []
+    for s in range(index_start, index_end):
+        res.append(max(prices[(s+1):(index_end+1)]) - prices[s])
+    return max(res)
+
+def bf_all(prices):
+    # best for 1 trade between index i and j
+    # pretty bad could be pythonified a bit
+    # O(l**2 * single_best) = O(l**4) pretty bad
+    S = []
+    l = len(prices)
+    for s in range(0, l-1):
+        n_l = [None] * l
+        for e in range(s+1, l):
+            #bf result
+            # for n_s in range(s, e):
+            #     res = max(prices[(n_s+1):(e+1)]) - prices[n_s]
+            #     m.append(res)
+            # n_l[e] = max(m)
+            n_l[e] = single_best(prices, s, e)
+        S.append(n_l)
+    return S
+
 
 def dyn_trader(prices, K):
+    # O(l**4) from init
+    # O(k l**2) => work on init
     N = len(prices)
-    m = np.zeros((K, N))
-    s = single_trade(prices)
-    # init
-    for i in range(2, N+1):
-        m[0, i] = bf_max(prices[:i])
-    for k in range(2, K+1):
-        for n in range(1, N-2):
-            m[k, n] = m[k-1, n] + s[n+1]
-    print(m)
+    optimal_nb_trades = 0
+    optimal_pl = 0
+    S = []
+    single = bf_all(prices)
+    S_init = single[0]
+    m = max((x for x in S_init if x is not None))
+    if m > optimal_pl:
+        m = optimal_pl
+        optimal_nb_trades = 1
+    S.append(S_init)
+    for k in range(1, K):
+        n_S = [None] * N # best en k trades en regardant les prix up to h
+        for end_trade in range(2*k+1, N):
+                n_S[end_trade] = max((S[k-1][h] + single[h+1][end_trade] for h in range(2*k-1, end_trade - 1)))
+        m = max((x for x in n_S if x is not None), default=None)
+        if (m is not None) and (m > optimal_pl):
+            optimal_pl = m
+            optimal_nb_trades = k
+        S.append(n_S)
+    return (S, optimal_nb_trades, optimal_pl)
 
 
 if __name__ == '__main__':
     prices = [7, 9, 10, 3]
-
     k = 4
     dyn_trader(prices, k)
--- pyalgo/test/test_play.py
+++ pyalgo/test/test_play.py
@@ -1,10 +1,22 @@
 import unittest
 
-from ..play.trade import single_trade
+from ..play.trade import bf_left
+from ..play.trade import bf_right
+from ..play.trade import dyn_trader
 
 class TestTrade(unittest.TestCase):
     def test_play(self):
         p = [4, 9, 1, 3, 8, 7, 1]
-        check_p = [5, -1, 7, 5, -1, -6, 0]
-        comp_p = single_trade(p)
-        self.assertEqual(check_p, comp_p)
+        check_k_1 = [None, 5, 5, 5, 7, 7, 7]
+        check_k_2 = [None, None, None, 7, 12, 12, 12]
+        check_k_3 = [None, None, None, None, None, 6, 6]
+        check_k_4 = [None, None, None, None, None, None, None]
+        res_k_1, o_n, o_v = dyn_trader(p, 1)
+        self.assertEqual(check_k_1, res_k_1[0])
+        res_k_2, o_n, o_v = dyn_trader(p, 2)
+        self.assertEqual(check_k_2, res_k_2[1])
+        res_k_3, o_n, o_v = dyn_trader(p, 3)
+        self.assertEqual(check_k_3, res_k_3[2])
+        res_k_4, o_n, o_v = dyn_trader(p, 4)
+        self.assertEqual(check_k_4, res_k_4[3])
+        print(o_n, o_v)
